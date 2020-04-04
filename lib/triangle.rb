7e75c80da922001f8332d444466cb2881c630752
Yeasha Hassan <tiram819@gmail.com>
2020-04-04 18:30:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ def initialize(a1, a2, a3)
  end 
  
  def kind 
-   if @a1 + @a2 < @a3 || @a1 + @a3 < @a2 || @a2 + @a3 < @a1 || (@a1 || @a2 || @a3) == 0
+   if (@a1 + @a2) < @a3 || (@a1 + @a3) < @a2 || (@a2 + @a3) < @a1 || (@a1 || @a2 || @a3) == 0
      begin
   raise TriangleError
 end
