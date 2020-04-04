227d4a4f92348c9f0ed4fbf0cb7570d1322d2898
Yeasha Hassan <tiram819@gmail.com>
2020-04-04 18:08:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ def initialize(a1, a2, a3)
  end 
  
  def kind 
-   if a1 == a2 && a2 == a3
+   if @a1 == @a2 && @a2 == @a3
    :equilateral
    elsif a1 == a2 && a1 != a3 || a1 == a3 && a1 != a2 || a2 == a3 && a2 != a1
    :isosceles
