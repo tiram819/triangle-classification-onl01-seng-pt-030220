36930ef0eb0a0b093ec50f73b616400ccac63a59
Yeasha Hassan <tiram819@gmail.com>
2020-04-04 18:23:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,9 @@ def initialize(a1, a2, a3)
  
  def kind 
    if @a1 + @a2 < @a3 || @a1 + @a3 < @a2 || @a2 + @a3 < @a1 || (@a1 || @a2 || @a3) == 0
-     
+     begin
+  raise YourCustomError
+end
    if @a1 == @a2 && @a2 == @a3
    :equilateral
    elsif @a1 == @a2 && @a1 != @a3 || @a1 == @a3 && @a1 != @a2 || @a2 == @a3 && @a2 != @a1
